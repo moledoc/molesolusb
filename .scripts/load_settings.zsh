fa17659b8e135f44dd21218c116318113c213e97
moledoc <meelis.utt@gmail.com>
2021-01-15 19:40:07 +0200

    made saving and loading of budgie,cinnamon and gnome settings easier

--- .scripts/load_settings.zsh
+++ .scripts/load_settings.zsh
@@ -2,22 +2,19 @@
 
 # README: load terminal, DE etc settings
 
-
 # load guake settings
 guake --restore-preferences $HOME/.config/guake/.guakeconf &
 
-# load gnome-terminal settings
-dconf load /org/gnome/terminal < $HOME/.config/gnome-terminal/gnome-terminal-settings
+# load gnome settings
+dconf load /org/gnome/ < $HOME/.config/gnome/gnome-settings
+# dconf load /org/gnome/terminal < $HOME/.config/gnome-terminal/gnome-terminal-settings
 
 DE=$(echo $DESKTOP_SESSION)
 
 if [ "$DE" = 'budgie-desktop' ]
 then
-  dconf load /com/solus-project/budgie-panel < $HOME/.config/budgieDE/panel-settings
-  dconf load /com/solus-project/budgie-raven < $HOME/.config/budgieDE/raven-settings
-  dconf load /com/solus-project/budgie-wm < $HOME/.config/budgieDE/wm-settings
+  dconf load /com/solus-project/ < $HOME/.config/budgieDE/budgie-settings
 elif [ "$DE" = 'cinnamon' ]
-  dconf load /org/cinnamon/desktop/keybindings/ < $HOME/.config/cinnamonDE/wm-settings
-  dconf load /org/cinnamon/theme/ < $HOME/.config/cinnamonDE/theme-settings
+  dconf load /org/cinnamon/ < $HOME/.config/cinnamonDE/cinnamon-settings
 fi
 
--- .scripts/save_settings.zsh
+++ .scripts/save_settings.zsh
@@ -6,18 +6,16 @@
 # save guake settings
 guake --save-preferences $HOME/.config/guake/.guakeconf &
 
-# save gnome-terminal settings
-dconf dump /org/gnome/terminal > $HOME/.config/gnome-terminal/gnome-terminal-settings
+# save gnome settings
+# dconf dump /org/gnome/terminal > $HOME/.config/gnome-terminal/gnome-terminal-settings
+dconf dump /org/gnome/ > $HOME/.config/gnome/gnome-settings
 
 DE=$(echo $DESKTOP_SESSION)
 
 if [ "$DE" = 'budgie-desktop' ]
 then
-  dconf dump /com/solus-project/budgie-panel > $HOME/.config/budgieDE/panel-settings
-  dconf dump /com/solus-project/budgie-raven > $HOME/.config/budgieDE/raven-settings
-  dconf dump /com/solus-project/budgie-wm > $HOME/.config/budgieDE/wm-settings
+  dconf dump /com/solus-project/ > $HOME/.config/budgieDE/budgie-settings
 elif [ "$DE" = 'cinnamon' ]
-  dconf dump /org/cinnamon/desktop/keybindings/ > $HOME/.config/cinnamonDE/wm-settings
-  dconf dump /org/cinnamon/theme/ > .config/cinnamonDE/theme-settings
+  dconf dump /org/cinnamon/ > $HOME/.config/cinnamonDE/cinnamon-settings
 fi
 
